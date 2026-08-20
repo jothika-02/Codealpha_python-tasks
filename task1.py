c953e538cdf775cd23b67c420a563f30392d71f7
jothika-02 <jothika2356@gmail.com>
2026-08-20 19:29:38 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,36 @@
+import random
+words = ["python", "computer", "coding", "program", "developer"]
+word = random.choice(words)
+guessed_letters = []
+wrong_guesses = 0
+max_wrong_guesses = 6
+print("===== HANGMAN GAME =====")
+print("Guess the word one letter at a time!")
+while wrong_guesses<max_wrong_guesses:
+    display_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("\nWord:", display_word)
+    print("Wrong guesses:", wrong_guesses, "/", max_wrong_guesses);
+    if all(letter in guessed_letters for letter in word):
+        print("Congratulations! You guessed the word!")
+        break
+    guess = input("Enter a letter: ").lower()
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter only one letter.")
+        continue
+    if guess in guessed_letters:
+        print("You already guessed that letter.") 
+        continue
+    guessed_letters.append(guess)
+    if guess in word:
+        print("Correct guess!")
+    else:
+        wrong_guesses += 1
+        print("Wrong guess!")
+else:
+    print("\nGame Over!")
+    print("The correct word was:", word)
